2fd7c090fd84d8543a69aca97b34456355939fd6
David Oliveira <davi.exe@hotmail.com>
2021-10-14 22:33:00 -0300

    Código evoluído mas não concluído

---
+++ ConsoleMyBankApp/ContaCorrente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMyBankApp
+{
+    class ContaCorrente
+    {
+
+        public Cliente titular;  // o titular é um cliente então deve ser do tipo Cliente
+        public int agencia;
+        public int conta;
+
+        private double saldo = 100.00;
+
+
+
+    }
+}
--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -6,7 +6,22 @@ namespace ConsoleMyBankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Cliente cliente = new Cliente("David de Oliveira", "000.000.000-00", "Analista");
+                     
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = cliente;
+            conta.agencia = 01;
+            conta.conta = 00001;
+
+            Console.WriteLine("##### - Conta Corrente Ativada - #####");
+            Console.WriteLine("Titular: {0}", conta.titular.nome);
+            Console.WriteLine("Agência: {0}", conta.agencia);
+            Console.WriteLine("Conta Corrente: {0}", conta.conta);
+
+
         }
     }
 }
